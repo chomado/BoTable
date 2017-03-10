ca0853166c4be56baedcad27c25531e6b8c69796
chomado <chomado@gmail.com>
2017-03-10 14:42:47 +0900

    全部 "th" だったのをそれっぽくした

--- Client/BoTable/BoTable/BoTable/Model/PartyInfo.cs
+++ Client/BoTable/BoTable/BoTable/Model/PartyInfo.cs
@@ -25,7 +25,12 @@ namespace BoTable.Model
         public int PartyCount
         {
             get { return this.partyCount; }
-            set { this.SetProperty(ref this.partyCount, value); this.OnPropertyChanged(nameof(PartyCountMessage)); }
+            set 
+            { 
+                this.SetProperty(ref this.partyCount, value); 
+                this.OnPropertyChanged(nameof(PartyCountMessage));
+                this.OnPropertyChanged(nameof(PartyCountMessageEn));
+            }
         }
 
         private int waitingMinutes;
@@ -33,14 +38,27 @@ namespace BoTable.Model
         public int WaitingMinutes
         {
             get { return this.waitingMinutes; }
-            set { this.SetProperty(ref this.waitingMinutes, value); this.OnPropertyChanged(nameof(WaitingMinutesMessage)); }
+            set 
+            { 
+                this.SetProperty(ref this.waitingMinutes, value); 
+                this.OnPropertyChanged(nameof(WaitingMinutesMessage));
+				this.OnPropertyChanged(nameof(WaitingMinutesMessageEn)); 
+            }
         }
 
 		public string PartyCountMessage => this.PartyCount == -1 ? "お客様は登録されていません" : $"あなたは {this.PartyCount} 組目です";
-        public string PartyCountMessageEn => this.PartyCount == -1 ? "You are not registered yet." : $"You are {this.PartyCount}th party.";
+        public string PartyCountMessageEn => this.PartyCount == -1 ? "You are not registered yet." : $"You are {this.PartyCount}{OrderingWord(this.partyCount)} party.";
 
 		public string WaitingMinutesMessage => this.PartyCount == -1 ? "" : $"だいたい {this.WaitingMinutes} 分待ちです";
         public string WaitingMinutesMessageEn => this.PartyCount == -1 ? "" : $"About {this.WaitingMinutes} min wait";
+
+        private string OrderingWord(int number)
+        {
+			if (number == 1) return "st";
+			if (number == 2) return "nd";
+            if (number == 3) return "rd";
+            return "th";
+        }
 
         public PartyInfo(Registration registration)
         {
