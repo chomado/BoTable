c957254b2e12c852ab571846846f2ed608161cfa
chomado <chomado@gmail.com>
2017-03-10 11:05:54 +0900

    レストランのメニュー画面を作った(とはいえ松屋しか無い)

---
+++ Client/BoTable/BoTable/BoTable/Model/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Prism.Mvvm;
+
+namespace BoTable
+{
+    public class Menu
+    {
+        public Menu()
+        {
+        }
+
+		public string Name { get; set; }
+        public string Description { get; set; }
+		public string ImageUrl { get; set; }
+        public int Price { get; set; }
+    }
+}
